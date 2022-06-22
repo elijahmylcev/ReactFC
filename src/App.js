f10b157d4c0560b007148c00f84a6d396ec9dfa3
Elijah Mylcev <elijahmylcev@gmail.com>
2022-06-22 21:45:33 +0300

    [stateValue, setStateValue]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [likes, setLikes] = useState(4);
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+  function decrement() {
+    setLikes(likes - 1);
+  }
   return (
-    <div className="App" />
+    <div className="App">
+      <h1>{likes}</h1>
+      <button type="submit" onClick={increment}>Increment</button>
+      <button type="submit" onClick={decrement}>Decrement</button>
+    </div>
   );
 }
 
