28cf40e0b7a3f2cc0c5a0c3cb65f3a3e6c7f4c0d
Elijah Mylcev <elijahmylcev@gmail.com>
2022-06-23 22:37:59 +0300

    Map for array

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
 import './App.css';
 import PostItem from './components/PostItem';
 
 function App() {
+  const [posts] = useState([
+    { id: 1, title: 'JavaScript - is programming language', body: 'description' },
+    { id: 2, title: 'JavaScript - is programming language', body: 'description' },
+    { id: 3, title: 'JavaScript - is programming language', body: 'description' },
+  ]);
   return (
     <div className="App">
       <Counter />
       <ClassCounter />
-      <PostItem post={{ id: 1, title: 'JavaScript - is programming language', body: 'description' }} />
+      <h1>Posts list</h1>
+      {posts.map((post) => <PostItem post={post} key={post.id} />)}
     </div>
   );
 }
