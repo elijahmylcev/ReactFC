d449fb19e1903d7b57864e55c0a9adeec5744196
Elijah Mylcev <elijahmylcev@gmail.com>
2022-06-24 20:49:51 +0300

    global useState() for post

--- src/App.js
+++ src/App.js
@@ -13,21 +13,14 @@ function App() {
     { id: 3, title: 'JavaScript - is programming language', body: 'description' },
   ]);
 
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+  const [post, setPost] = useState({ title: '', body: '' });
+
   const addNewPost = (e) => {
     e.preventDefault();
 
-    const newPost = {
-      id: Date.now(),
-      title,
-      body,
-    };
+    setPosts([...posts, { ...post, id: Date.now() }]);
 
-    setPosts([...posts, newPost]);
-
-    setTitle('');
-    setBody('');
+    setPost({ title: '', body: '' });
   };
 
   return (
@@ -38,15 +31,15 @@ function App() {
       <form action="submit">
         {/* Управляемый компонент */}
         <MyInput
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          value={post.title}
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
           type="text"
           placeholder="Title of post"
         />
         {/* Неуправляемый, неконтролируемый компонент */}
         <MyInput
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
+          value={post.body}
+          onChange={(e) => setPost({ ...post, body: e.target.value })}
           type="text"
           placeholder="Description"
         />
